50413f59919ca893e8be5ce01a36b865c6eb8e67
Michael Scott Shappe <mshappe@camelopard-consulting.com>
2011-10-17 21:41:29 -0500

    OK, no sessions, yet, but more unit tests for User.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,11 @@
 class User < ActiveRecord::Base    
-    validates :name, presence: true, allow_blank: false, uniqueness: true
-    validates :realname, presence: true, allow_blank: true
+    name_regex = /^[a-zA-Z0-9_]*$/
+    
+    validates :name, presence: true, 
+                     allow_blank: false, 
+                     uniqueness: true, 
+                     length: { maximum: 32 },
+                     format: { with: name_regex }
+    validates :realname, presence: true, allow_blank: true, length: { maximum: 64 }
     has_secure_password
 end
