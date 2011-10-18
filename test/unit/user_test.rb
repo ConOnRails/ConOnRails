4fc15825fdef5313af934b75b168aae2731e98c3
Michael Scott Shappe <mshappe@camelopard-consulting.com>
2011-10-17 21:41:29 -0500

    OK, no sessions, yet, but more unit tests for User.
    Former-commit-id: 50413f59919ca893e8be5ce01a36b865c6eb8e67

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -3,6 +3,17 @@ require 'test_helper'
 class UserTest < ActiveSupport::TestCase
   fixtures :users
   
+  def failure_is_good( input_attributes )
+    user = nil
+    if input_attributes == {}
+      user = User.new
+    else
+      user = User.create( input_attributes )
+    end
+
+    assert user.invalid?
+  end
+  
   setup do
     @input_attributes = {
         name: "mikey",
@@ -13,23 +24,34 @@ class UserTest < ActiveSupport::TestCase
   end
   
   test "user attributes must not be empty" do
-    user = User.new
-    assert user.invalid?
-    assert user.errors[:name].any?
+    empty = {}
+    failure_is_good empty
   end
   
   test "create a new user" do
-    User.create!(@input_attributes)
+    User.create! @input_attributes
   end
     
   test "user's name should be unique" do
     user1 = User.create!(@input_attributes)
-    user2 = User.create(@input_attributes)
-    assert user2.invalid?, "This should have failed"
-    assert user2.errors[:name].any?
+    failure_is_good @input_attributes
   end
   
-  # test "the truth" do
-  #   assert true
-  # end
+  test "username should not be longer than 32 characters" do
+    long_name_is_long = @input_attributes
+    long_name_is_long[:name] = 'a' * 33
+    failure_is_good long_name_is_long
+  end
+  
+  test "realname should not be longer than 64 characters" do
+    long_name_is_long = @input_attributes
+    long_name_is_long[:realname] = 'a' * 65
+    failure_is_good long_name_is_long
+  end
+  
+  test "username should only contain alphanumerics and underscores" do
+    bad_username_is_bad = @input_attributes
+    bad_username_is_bad[:name] = "a&%T@%{ \003} elknart"
+    failure_is_good bad_username_is_bad
+  end
 end
