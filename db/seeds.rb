88be7d7e24ca67adbf2c0091f38ef6a16d4d030c
Michael Scott Shappe <mshappe@camelopard-consulting.com>
2011-10-17 22:05:01 -0500

    Add an index User table; ensure seeds validate correctly (and throw exceptions if they don't).
    Former-commit-id: 96b3bd6c77b7a26fa0e6a0e1c37397f14f5c111b

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,50 +12,50 @@ EventStatus.delete_all
 EventType.delete_all
 User.delete_all
 
-user = User.create( { name: "Uncle Mikey",
-                        realname: "Michael Scott Shappe"
-                    } )
-                 
-                   
+user = User.create!( { name: "Uncle_Mikey",
+  realname: "Michael Scott Shappe",
+  password: "zog",
+  password_confirmation: "zog"
+  } )
 
-statuses = EventStatus.create( [ { tag: 'active', name: 'Active' },
-                                 { tag: 'dup', name: 'Duplicate' },
-                                 { tag: 'resolved', name: 'Resolved' }
-                               ] )
-types = EventType.create( [ { tag: 'general', name: 'General' },
-                            { tag: 'emerg', name: 'Emergency' },
-                            { tag: 'med', name: 'Medical' },
-                            { tag: 'followup', name: 'Post Con Followup' },
-                            { tag: 'quote', name: 'Quote' }
-                          ] )
 
-p statuses
+statuses = EventStatus.create!( 
+  [ { tag: 'active', name: 'Active' },
+    { tag: 'dup', name: 'Duplicate' },
+    { tag: 'resolved', name: 'Resolved' } ] )
+types = EventType.create!( 
+  [ { tag: 'general', name: 'General' },  { tag: 'emerg', name: 'Emergency' },
+    { tag: 'med', name: 'Medical' },
+    { tag: 'followup', name: 'Post Con Followup' },
+    { tag: 'quote', name: 'Quote' } ] )
+
 event_status = statuses[0]
 event_type = types[0]
 
-event = Event.create( { summary: "Sit right down and hear a tale of a fateful trip",
-                          event_status: event_status,
-                          event_type: event_type
-                      } )
+event = Event.create!( 
+  { summary: "Sit right down and hear a tale of a fateful trip",
+    event_status: event_status,
+    event_type: event_type } )
 
-Entry.create( { user: user,
-                  event: event,
-                  description: 
-                  %{
-Never let the llamas loose.
-Never ever ever.
-Never let the llamas loose.
-Even wearing leather.
-} } )
-Entry.create( {
-                  user: user,
-                  event: event,
-                  description:
-                  %{
-Kawaita hitome de
-Dare katma itekure
-The real folk blues
-Honto no kana shimi ga
-Shiritaidake
-} } )
+Entry.create!( 
+  { user: user,
+    event: event,
+    description: 
+          %{
+            Never let the llamas loose.
+            Never ever ever.
+            Never let the llamas loose.
+            Even wearing leather.
+            } } )
+Entry.create!( 
+  { user: user,
+    event: event,
+    description:
+              %{
+                Kawaita hitome de
+                Dare katma itekure
+                The real folk blues
+                Honto no kana shimi ga
+                Shiritaidake
+                } } )
 
