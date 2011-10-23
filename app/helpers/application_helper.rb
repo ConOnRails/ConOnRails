bf40130ce0d7890c7943b266a2756b3b2bb1a486
Michael Scott Shappe <mshappe@camelopard-consulting.com>
2011-10-22 21:10:55 -0500

    Top-level layout changes to begin to align with DeNae's design

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,8 @@
 module ApplicationHelper
   
   def title
-    base_title = "Con On Rails"
-    if @title.nil?
-      base_title
-    else
-      "#{base_title} | #{ @title }"
+    if not @title.nil?
+      " | #{ @title }"
     end
   end
 
