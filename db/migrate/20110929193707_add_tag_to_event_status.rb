c30fb63cdf9373333195a784f358084854b49d70
Michael Scott Shappe <mshappe@camelopard-consulting.com>
2011-10-22 21:21:35 -0500

    More event type/status cleanup
    Former-commit-id: 3614844f6da32705a5c6f3fbd95a302616c9715f

--- db/migrate/20110929193707_add_tag_to_event_status.rb
+++
@@ -1,5 +0,0 @@
-class AddTagToEventStatus < ActiveRecord::Migration
-  def change
-    add_column :event_statuses, :tag, :string
-  end
-end
--- db/migrate/20110929193724_add_tag_to_event_type.rb
+++
@@ -1,5 +0,0 @@
-class AddTagToEventType < ActiveRecord::Migration
-  def change
-    add_column :event_types, :tag, :string
-  end
-end
