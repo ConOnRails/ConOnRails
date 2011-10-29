2e5bc0f2c3c7a7115c34dd4665db26a0d28cb5bc
Michael Scott Shappe <mshappe@camelopard-consulting.com>
2011-10-29 14:26:44 -0500

    Better event magic. TODO: recreate entry forms.

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,21 +1,13 @@
 class Event < ActiveRecord::Base
     has_many   :entries, dependent: :destroy
     accepts_nested_attributes_for :entries, allow_destroy: true
-    
-    # Most uses of Rails nested attributes and routes assume that
-    # the parent object is created and saved before children are
-    # added. We're not doing it that way, so we create a temporary
-    # attribute, 'firstdescription' we can throw onto the form, and
-    # then the controller uses it to build the first entry.
-    #
-    attr_accessor :firstdescription
 
     STATUSES = [ 'Active', 'Closed' ]
 
     def get_num_entries
         return entries.count
     end
-    
+  
     def status
       return 'Active' if is_active?
       return 'Closed' unless is_active?
