9e808e25468c6b5fd710977e1e77efd5876f9e48
Michael Scott Shappe <mshappe@camelopard-consulting.com>
2011-11-18 14:32:24 -0600

    Lost and Found. Lots of unit testing to ensure good validation for this one.
    Former-commit-id: 0c1b0b2eeb40013d5f0d6d9fab3f5176b29d90bb

--- test/unit/lost_and_found_item_test.rb
+++ test/unit/lost_and_found_item_test.rb
@@ -9,33 +9,86 @@ class LostAndFoundItemTest < ActiveSupport::TestCase
     @returned = lost_and_found_items(:returned)
   end
 
-  test "cannot create lost item without cat, desc, last seen, flags false" do
+  test "default construction should be invalid" do
     lfi = LostAndFoundItem.create
-    assert lfi.invalid?
+    assert lfi.invalid?, "Vole"
     assert lfi.errors[:category].any?
     assert lfi.errors[:description].any?
-    assert lfi.errors[:where_last_seen].any?
+    assert lfi.errors[:reported_missing].any?
     assert lfi.errors[:found].any?
-    assert lfi.errors[:returned].any?
   end
-
-  test "cannot create found item without cat, desc, where found, found true" do
-    lfi = LostAndFoundItem.create
+  
+  test "cannot create an item that is both missing and found" do
+    # Note: this is ONLY a workflow issue on create. Items can be UPDATED as
+    # either missing or found if created with the other, and then both will
+    # be true, but an item can only be CREATED as missing or found
+    attrs = @lost.attributes
+    attrs[:found] = true
+    lfi = LostAndFoundItem.create(attrs)
     assert lfi.invalid?
-    assert lfi.errors[:category].any?
-    assert lfi.errors[:description].any?
-    assert lfi.errors[:where_found].any?
+    assert lfi.errors[:reported_missing].any?
     assert lfi.errors[:found].any?
-    assert lfi.errors[:returned].any?
   end
-
-  test "lost item must have where_last_seen" do
+  
+  test "item created missing MUST have where_last_seen owner_name owner_contact" do
     doom = @lost.attributes
     doom.delete "where_last_seen"
+    doom.delete "owner_name"
+    doom.delete "owner_contact"
+    lfi = LostAndFoundItem.create doom
+    assert lfi.invalid?
+    assert lfi.errors[:where_last_seen].any?
+    assert lfi.errors[:owner_name].any?
+    assert lfi.errors[:owner_contact].any?
+  end
+  
+  test "item created missing MUST NOT have where_found" do
+    doom = @lost.attributes
+    doom[:where_found] ="Doomland"
+    lfi = LostAndFoundItem.create doom
+    assert lfi.invalid?
+    assert lfi.errors[:where_found].any?
+  end
+  
+  test "item created found MUST have where_found" do
+    doom = @found.attributes
+    doom.delete "where_found"
+    lfi = LostAndFoundItem.create doom
+    assert lfi.invalid?
+    assert lfi.errors[:where_found]
+  end
+  
+  test "item created found MUST NOT have where_last_seen" do
+    doom = @found.attributes
+    doom[:where_last_seen] = "Doomland"
     lfi = LostAndFoundItem.create doom
     assert lfi.invalid?
     assert lfi.errors[:where_last_seen].any?
   end
-
   
+  test "item updated missing MUST have where_last_seen other fields irrelevant" do
+    doom = @found.attributes
+    lfi = LostAndFoundItem.create!(doom)
+    lfi.reported_missing = true
+    lfi.save
+    assert lfi.invalid?
+    assert lfi.errors[:where_last_seen].any?
+  end
+  
+  test "item updated found MUST have where_found other fields irrelevant" do
+    doom = @lost.attributes
+    lfi = LostAndFoundItem.create(doom)
+    lfi.found = true
+    lfi.save
+    assert lfi.invalid?
+    assert lfi.errors[:where_found].any?
+  end
+
+  test "category must be valid" do
+    doom = @lost.attributes
+    doom["category"] = "Llama" # This is only valid right now
+    lfi = LostAndFoundItem.create(doom)
+    assert lfi.invalid?
+    assert lfi.errors[:category].any?
+  end
 end
