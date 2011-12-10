7442681dcf538d28d61381c926008a61d7bb1b1c
Michael Scott Shappe <mshappe@camelopard-consulting.com>
2011-12-10 16:27:36 -0600

    Keywords field now searches descriptions.
    Note: doesn't also search details, yet.

--- app/controllers/lost_and_found_items_controller.rb
+++ app/controllers/lost_and_found_items_controller.rb
@@ -1,17 +1,45 @@
 class LostAndFoundItemsController < ApplicationController
+  attr_reader :lfis
+  
   def searchform
   end
 
   def search
     categories = build_categories_from_params
+    keywords = []
     
+    if params.has_key? :keywords and params[:keywords] != ''
+      keywords = params[:keywords].split
+    end
+    
+    p keywords
+    p params[:search_type]
+    
+    like = ""
+    if params[:search_type] == 'all'
+      keywords.each do |k|
+        like += "%#{k}%"
+      end
+      like = "\"description\" LIKE '#{like}'"
+    elsif params[:search_type] == 'any'
+      keywords.each do |k|
+        if k == keywords.first
+          like = "\"description\" LIKE '%#{k}%'"
+        else
+          like += " OR \"description\" LIKE '%#{k}%'"
+        end
+      end
+    end
+    
+    p like
+          
     @lfis = LostAndFoundItem.where returned: false
-    @lfis = @lfis.where( "description LIKE ?", params[:keywords] ) if params.has_key? :keywords and params[:keywords] != ''
+    @lfis = @lfis.where( like ) 
     @lfis = @lfis.where( category: categories ) unless categories == []
     
     respond_to do |format|
-      format.html { render "index"}
-      format.json { render json: @lfis}
+      format.html { render 'index' }
+      format.json { render json: @lfis } 
     end    
   end
 
