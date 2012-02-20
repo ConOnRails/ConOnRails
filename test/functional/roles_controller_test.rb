6fece02beea825b4205c9029fd4df091f92a81b7
Michael Scott Shappe <mshappe@camelopard-consulting.com>
2012-02-20 15:07:50 -0600

    Earliest support for roles. Doesn't do much yet.

--- test/functional/roles_controller_test.rb
+++ test/functional/roles_controller_test.rb
@@ -2,46 +2,48 @@ require 'test_helper'
 
 class RolesControllerTest < ActionController::TestCase
   setup do
-    @role = roles(:one)
+    @peon = roles(:peon)
+    @admin = roles(:admin)
+    @user = users :one
   end
 
   test "should get index" do
-    get :index
+    get :index, {}, { user_id: @user.id }
     assert_response :success
     assert_not_nil assigns(:roles)
   end
 
   test "should get new" do
-    get :new
+    get :new, {}, { user_id: @user.id }
     assert_response :success
   end
 
   test "should create role" do
     assert_difference('Role.count') do
-      post :create, role: @role.attributes
+      post :create,  { role: @peon.attributes }, { user_id: @user.id }
     end
 
     assert_redirected_to role_path(assigns(:role))
   end
 
   test "should show role" do
-    get :show, id: @role.to_param
+    get :show, { id: @peon.to_param }, { user_id: @user.id }
     assert_response :success
   end
 
   test "should get edit" do
-    get :edit, id: @role.to_param
+    get :edit, { id: @peon.to_param }, { user_id: @user.id }
     assert_response :success
   end
 
   test "should update role" do
-    put :update, id: @role.to_param, role: @role.attributes
+    put :update, { id: @peon.to_param }, { role: @peon.attributes, user_id: @user.id }
     assert_redirected_to role_path(assigns(:role))
   end
 
   test "should destroy role" do
     assert_difference('Role.count', -1) do
-      delete :destroy, id: @role.to_param
+      delete :destroy, { id: @peon.to_param }, { user_id: @user.id }
     end
 
     assert_redirected_to roles_path
