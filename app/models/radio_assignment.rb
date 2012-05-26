2998b271e36cac9128a842ac86c73e5d9fc988f4
Michael Scott Shappe <mshappe@camelopard-consulting.com>
2012-05-26 12:49:30 -0500

    Radio forms look a bit better. Now validating department allotments!
    Former-commit-id: f85a688c3d116c3d8df32350ad1eeb7c3c2d049d

--- app/models/radio_assignment.rb
+++ app/models/radio_assignment.rb
@@ -1,7 +1,21 @@
+class DepartmentAllotmentChecker < ActiveModel::Validator
+  def validate(record)
+     if RadioAssignment.department_count(record.department.id) + 1 >
+         record.department.radio_allotment
+       record.errors[:department] << "All of this department's radios are allotted"
+     end
+  end
+end
+
 class RadioAssignment < ActiveRecord::Base
   belongs_to :radio
   belongs_to :volunteer
   belongs_to :department
   validates_presence_of :radio, :volunteer, :department
   validates_uniqueness_of :radio_id # Only one instance of a radio checkout at a time!
+  validates_with DepartmentAllotmentChecker
+
+  def RadioAssignment.department_count(dept_id)
+    RadioAssignment.where( department_id: dept_id ).count
+  end
 end
