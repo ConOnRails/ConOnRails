7ddad700a94e4ccfdc86550f0f0ed1da33503157
Michael Scott Shappe <mshappe@camelopard-consulting.com>
2012-06-07 20:59:21 -0500

    Make sure everything that isn't itself a specialized audit table is audited, and all right near the top of the model.
    Former-commit-id: 1dbb116cef9aba77f3f49a8770fc8d50064fe2c4

--- app/models/department.rb
+++ app/models/department.rb
@@ -1,4 +1,5 @@
 class Department < ActiveRecord::Base
+  audited
   belongs_to :volunteer
   belongs_to :radio_group
   has_many :duty_board_slots
@@ -6,7 +7,6 @@ class Department < ActiveRecord::Base
   validates_presence_of :volunteer
   validates_presence_of :radio_group
   validates_uniqueness_of :name
-  audited
 #  validates_associated :volunteer
   # validates :not_too_many
 end
