f42b8f9095a9c63e37de4e76532ec2a078a4eecb
Michael Scott Shappe <mshappe@camelopard-consulting.com>
2012-06-10 09:00:37 -0500

    Duty board name is now freeform rather than a volunteer link. Beginning of volunteer/user linkage

--- app/models/duty_board_assignment.rb
+++ app/models/duty_board_assignment.rb
@@ -1,9 +1,8 @@
 class DutyBoardAssignment < ActiveRecord::Base
   audited
 
-  belongs_to :volunteer
   belongs_to :duty_board_slot
-  validates_presence_of :volunteer, message: "You must associate with a volunteer"
+  validates_presence_of :name, message: "You must have a name"
   validates_presence_of :duty_board_slot, message: "You must associate with a duty board slot"
   validates_length_of :notes, maximum: 255
 end
