ff6d13859bcc36fe9b89a3d47671547ae7de1580
Michael Scott Shappe <michael@wescrimmage.com>
2012-12-30 15:03:54 -0500

    More test cleanup. Tests now work in RubyMine again (gave up on minitest for now, using test-unit).
    Former-commit-id: 0218bcdd18c064ea92bca1996398d4166f6cba76

--- app/models/duty_board_slot.rb
+++ app/models/duty_board_slot.rb
@@ -1,5 +1,5 @@
 class DutyBoardSlot < ActiveRecord::Base
-  attr_accessible :name, :duty_board_group_id
+  attr_accessible :name, :duty_board_group, :duty_board_group_id
   audited
 
   belongs_to :duty_board_group
--- app/models/role.rb
+++ app/models/role.rb
@@ -1,5 +1,7 @@
 class Role < ActiveRecord::Base
-  attr_accessible :name, :write_entries
+  attr_accessible :name, :write_entries, :add_lost_and_found, :admin_duty_board, :admin_radios, :admin_schedule,
+                  :admin_users, :assign_duty_board_slots, :assign_radios, :assign_shifts, :modify_lost_and_found,
+                  :read_hidden_entries, :make_hidden_entries, :read_audits, :rw_secure
 
   audited
   has_and_belongs_to_many :users
