aa9160aeb2145c235f7374ec45b198ab8fab343d
Michael Scott Shappe <michael@wescrimmage.com>
2012-12-30 21:16:02 -0500

    Fix a migration that breaks a clean install
    Former-commit-id: e346be3f74d634410a5652406eea5da59761247b

--- db/migrate/20120506041011_create_radio_assignments.rb
+++ db/migrate/20120506041011_create_radio_assignments.rb
@@ -3,6 +3,7 @@ class CreateRadioAssignments < ActiveRecord::Migration
     create_table :radio_assignments do |t|
       t.integer :radio_id
       t.integer :volunteer_id
+      t.string :state # This moves to radios in a later migration
 
       t.timestamps
     end
