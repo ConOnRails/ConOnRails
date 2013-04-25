b5d6e4b184eb67c5ce4645bcdb201934a0d3ff7c
Michael Scott Shappe <michael@wescrimmage.com>
2013-04-25 13:04:50 -0500

    OK, so now, I think I've got decent way to generate a fake Attendee database. Yay?

--- test/unit/attendee_test.rb
+++ test/unit/attendee_test.rb
@@ -2,15 +2,13 @@ require 'test_helper'
 
 class AttendeeTest < ActiveSupport::TestCase
   setup do
-#    Attendee.all.each() do |a|
-#     print "#{a.FIRST_NAME}\n"
-#    end
+    @attendee = FactoryGirl.build(:attendee)
   end
 
   test "can get composite name" do
-    skip "Cannot test in this environment" unless Attendee.connected?
-    a = Attendee.find_by_LAST_NAME( "Shappe" )
-    assert_equal "Michael Scott Shappe", a.name
+    assert_equal @attendee.FIRST_NAME + ' ' +
+      ( @attendee.MIDDLE_NAME.blank? ? '' : @attendee.MIDDLE_NAME + ' ' ) +
+      @attendee.LAST_NAME, @attendee.name
   end
 
 end
