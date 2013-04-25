668e1309fb6cc414ade3a27fa23f10d1f6ff056a
Michael Scott Shappe <michael@wescrimmage.com>
2013-04-25 13:04:50 -0500

    OK, so now, I think I've got decent way to generate a fake Attendee database. Yay?

--- app/models/attendee.rb
+++ app/models/attendee.rb
@@ -1,16 +1,12 @@
 class Attendee < ActiveRecord::Base
-  begin
-    establish_connection :attendees
-  rescue => e
-    Rails.logger.warn "Attendee database not connected: #{e.message}"
-  end
+  establish_connection :attendees
 
   self.table_name = "2012"
   self.primary_key = "attendee_id"
 
   def name
-    return nil unless self.present? and self.connected?
-    return self.FIRST_NAME + " " +
+    return nil unless self.present?
+    self.FIRST_NAME + " " +
         ( self.MIDDLE_NAME.blank? ? "" : self.MIDDLE_NAME + " " ) +
         self.LAST_NAME
   end
