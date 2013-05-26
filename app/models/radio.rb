e2bf37f16f2c9db54e49bad9ba131b363fd1acb9
Michael Scott Shappe <mshappe@camelopard-consulting.com>
2013-05-25 19:28:20 -0500

    Only test that failed for Postgres (because of an old query in a scope)
    Former-commit-id: bb5faca37bea5817a8a0b1107ba7c4c3814b74b5

--- app/models/radio.rb
+++ app/models/radio.rb
@@ -23,8 +23,8 @@ class Radio < ActiveRecord::Base
   validates_presence_of :number
   validates_inclusion_of :state, in: %w( in out retired )
   validates_uniqueness_of :number, scope: :radio_group_id
-  scope :assigned, where('state = "out"')
-  scope :unassigned, where('state = "in"')
+  scope :assigned, where { |r| r.state == 'out' }
+  scope :unassigned, where { |r| r.state == 'in' }
 
   def name
     self.number
