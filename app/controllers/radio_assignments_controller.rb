36cbc3de41ba6ef63ef907ff46dd4c4d506c0c16
Michael Scott Shappe <mshappe@camelopard-consulting.com>
2013-06-22 14:32:10 -0500

    Radio assignment transfers!
    Former-commit-id: 830d06aa9c6a34bd369498b4f4d6c4db4d4b313a

--- app/controllers/radio_assignments_controller.rb
+++ app/controllers/radio_assignments_controller.rb
@@ -1,33 +1,36 @@
 class RadioAssignmentsController < ApplicationController
+  respond_to :html, :json
+
   before_filter :can_assign_radios?
+  before_filter :find_assignment
+
 
   # POST /radio_assignments
   # POST /radio_assignments.json
   def create
-    @radio_assignment             = RadioAssignment.new(params[:radio_assignment])
-    @radio_assignment.radio.state = "out"
-    respond_to do |format|
-      if  @radio_assignment.save and
-          @radio_assignment.radio.save and
-          RadioAssignmentAudit.audit_checkout(@radio_assignment, current_user)
-        format.html { redirect_to radios_url, notice: 'Radio assignment was successfully created.' }
-        format.json { render json: @radio_assignment, status: :created, location: @radio_assignment }
-      else
-        format.html { redirect_to checkout_radio_url(@radio_assignment.radio), notice: "Radio not assigned. #{@radio_assignment.errors[:department].first}" }
-        format.json { render json: @radio_assignment.errors, status: :unprocessable_entity }
-      end
-    end
+    @radio_assignment = RadioAssignment.checkout(params[:radio_assignment], current_user)
+    respond_with @radio_assignment, location: radios_url
   end
 
-  # DELETE /radio_assignments/1
-  # DELETE /radio_assignments/1.json
+  def update
+    @radio_assignment.transfer params[:radio_assignment], current_user
+    respond_with @radio_assignment, location: radios_url
+  end
+
+# DELETE /radio_assignments/1
+# DELETE /radio_assignments/1.json
   def destroy
-    @radio_assignment = RadioAssignment.find(params[:id])
     @radio_assignment.checkin current_user
 
     respond_to do |format|
       format.html { redirect_to radios_url }
       format.json { head :ok }
     end
+  end
+
+  protected
+
+  def find_assignment
+    @radio_assignment = RadioAssignment.find(params[:id]) if params[:id]
   end
 end
