4261b1661945b3cbd38bc3fafd678c61470ac355
Michael Scott Shappe <mshappe@camelopard-consulting.com>
2013-06-22 18:45:24 -0500

    Controller Cleanup 2

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,36 +1,23 @@
 class MessagesController < ApplicationController
+  respond_to :html, :json
+
   # GET /messages
   # GET /messages.json
   def index
-    @qc = Contact.search params[:qc]
-    @messages = Message.where( is_active: true ).page(params[:page])
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @messages }
-    end
+    @qc       = Contact.search params[:qc]
+    @messages = Message.where(is_active: true).page(params[:page])
   end
 
   # GET /messages/1
   # GET /messages/1.json
   def show
     @message = Message.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @message }
-    end
   end
 
   # GET /messages/new
   # GET /messages/new.json
   def new
     @message = Message.new(params[:message])
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @message }
-    end
   end
 
   # GET /messages/1/edit
@@ -41,34 +28,18 @@ class MessagesController < ApplicationController
   # POST /messages
   # POST /messages.json
   def create
-    @message = Message.new(params[:message])
+    @message      = Message.new(params[:message])
     @message.user = current_user
-
-    respond_to do |format|
-      if @message.save
-        format.html { redirect_to messages_path, notice: 'Message was successfully created.' }
-        format.json { render json: @message, status: :created, location: @message }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @message.errors, status: :unprocessable_entity }
-      end
-    end
+    flash[:notice] = 'Message was successfully created.' if @message.save
+    respond_with @message, location: messages_path
   end
 
   # PUT /messages/1
   # PUT /messages/1.json
   def update
     @message = Message.find(params[:id])
-
-    respond_to do |format|
-      if @message.update_attributes(params[:message])
-        format.html { redirect_to messages_path, notice: 'Message was successfully updated.' }
-        format.json { head :ok }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @message.errors, status: :unprocessable_entity }
-      end
-    end
+    flash[:notice] = 'Message was successfully updated.' if @message.update_attributes(params[:message])
+    respond_with @message, location: messages_path
   end
 
   # DELETE /messages/1
@@ -76,15 +47,6 @@ class MessagesController < ApplicationController
   def destroy
     @message = Message.find(params[:id])
     @message.destroy
-
-    respond_to do |format|
-      format.html { redirect_to messages_url }
-      format.json { head :ok }
-    end
-  end
-
-  def close
-    params[:message] = { is_active: false }
-    update( )
+    respond_with @message, location: messages_path
   end
 end
--- app/controllers/radio_groups_controller.rb
+++ app/controllers/radio_groups_controller.rb
@@ -2,41 +2,29 @@ class RadioGroupsController < ApplicationController
   before_filter :can_admin_radios?, only: [:new, :create, :edit, :update, :destroy]
   before_filter :can_assign_radios?, only: [:index, :show]
 
+  respond_to :html, :json
+
   # GET /radio_groups
   # GET /radio_groups.json
-  def index( del_or_edit = "edit")
+  def index(del_or_edit = "edit")
     @radio_groups = RadioGroup.all
     @del_or_edit  = del_or_edit
-    respond_to do |format|
-      format.html { render "index"} # index.html.erb
-      format.json { render json: @radio_groups }
-    end
   end
 
   def delindex
-    index( "delete" )
+    index("delete")
   end
 
   # GET /radio_groups/1
   # GET /radio_groups/1.json
   def show
     @radio_group = RadioGroup.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @radio_group }
-    end
   end
 
   # GET /radio_groups/new
   # GET /radio_groups/new.json
   def new
     @radio_group = RadioGroup.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @radio_group }
-    end
   end
 
   # GET /radio_groups/1/edit
@@ -48,32 +36,16 @@ class RadioGroupsController < ApplicationController
   # POST /radio_groups.json
   def create
     @radio_group = RadioGroup.new(params[:radio_group])
-
-    respond_to do |format|
-      if @radio_group.save
-        format.html { redirect_to @radio_group, notice: 'Radio group was successfully created.' }
-        format.json { render json: @radio_group, status: :created, location: @radio_group }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @radio_group.errors, status: :unprocessable_entity }
-      end
-    end
+    flash[:notice] = 'Radio group was successfully created.' if @radio_group.save
+    respond_with @radio_group
   end
 
   # PUT /radio_groups/1
   # PUT /radio_groups/1.json
   def update
     @radio_group = RadioGroup.find(params[:id])
-
-    respond_to do |format|
-      if @radio_group.update_attributes(params[:radio_group])
-        format.html { redirect_to @radio_group, notice: 'Radio group was successfully updated.' }
-        format.json { head :ok }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @radio_group.errors, status: :unprocessable_entity }
-      end
-    end
+    flash[:notice] = 'Radio group was successfully updated.' if @radio_group.update_attributes(params[:radio_group])
+    respond_with @radio_group
   end
 
   # DELETE /radio_groups/1
@@ -81,10 +53,6 @@ class RadioGroupsController < ApplicationController
   def destroy
     @radio_group = RadioGroup.find(params[:id])
     @radio_group.destroy
-
-    respond_to do |format|
-      format.html { redirect_to radio_groups_url }
-      format.json { head :ok }
-    end
+    respond_with @radio_group, location: radio_groups_path
   end
 end
