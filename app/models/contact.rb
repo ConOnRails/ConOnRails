b63fddf57b8ccbba3af8316e9e1992fa108213fd
Michael Scott Shappe <mshappe@camelopard-consulting.com>
2013-06-22 15:31:18 -0500

    CodeClimate-flagged issue.

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -20,6 +20,6 @@ class Contact < ActiveRecord::Base
   audited
   validates_format_of :cell_phone,
                       message: "must be a valid telephone number.",
-                      with:    /^[\(\)0-9\- \+\.]{10,20}$/
+                      with:    /\A[\(\)0-9\- \+\.]{10,20}\z/
   validates :name, presence: true, allow_blank: false
 end
