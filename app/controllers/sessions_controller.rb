34d6f6d02369d854d54eeb42ea3f83604356d7c9
Michael Scott Shappe <mshappe@camelopard-consulting.com>
2013-06-22 19:06:16 -0500

    Cleanup sessions a bit.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,9 @@ class SessionsController < ApplicationController
   skip_before_filter :require_login, only: [:new, :create, :getroles]
 
   private
-  def ip() request.env['HTTP_X_FORWARDED_FOR'] || request.remote_ip end
+  def ip()
+    request.env['HTTP_X_FORWARDED_FOR'] || request.remote_ip
+  end
 
   public
 
@@ -13,19 +15,17 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by_name(params[:name])
     if user && user.authenticate(params[:password])
-      reset_session
-      session[:user_id] = user.id
-      session[:current_role] = params[:role]
-      LoginLog.create! user_name: user.name, role_name: params[:role], comment: :success, ip: ip()
-      redirect_to root_url, notice: "Logged in!"
+      setup_session user
+      flash[:notice] =  "Logged in!"
     else
-      LoginLog.create! user_name: params[:name], role_name: params[:role], comment: :failure, ip: ip()
-      redirect_to root_url, notice: "Invalid username or password"
+      log_failure
+      flash[:notice] = "Invalid username or password"
     end
+    redirect_to root_url
   end
 
   def destroy
-    @title            = "Goodbye!"
+    @title = "Goodbye!"
     LoginLog.create! user_name: (current_user ? current_user.name : "nobody"),
                      role_name: current_role, comment: :logout, ip: ip
     session[:user_id] = nil
@@ -38,5 +38,18 @@ class SessionsController < ApplicationController
     rescue NoMethodError
       @rolenames = []
     end
+  end
+
+  protected
+
+  def setup_session(user)
+    reset_session
+    session[:user_id]      = user.id
+    session[:current_role] = params[:role]
+    LoginLog.create! user_name: user.name, role_name: params[:role], comment: :success, ip: ip()
+  end
+
+  def log_failure
+    LoginLog.create! user_name: params[:name], role_name: params[:role], comment: :failure, ip: ip()
   end
 end
