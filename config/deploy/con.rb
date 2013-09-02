e2188af82025261479bed42af5f78f04f6267170
Michael Scott Shappe <mshappe@camelopard-consulting.com>
2013-09-02 17:13:51 -0500

    A couple of outstanding tweaks.
    Former-commit-id: 52714b6d6b177c3de40758ec4af29b2635844059

--- config/deploy/con.rb
+++ config/deploy/con.rb
@@ -1,5 +1,5 @@
-set :deploy_to, "/home/ops/apps/#{application}"
-set :user, 'ops'
-set :group, 'ops'
-set :rvm_type, :user
-server '192.168.92.136', :app, :web, :db, :primary => true
+set :deploy_to, "/home/cvgops/apps/#{application}"
+set :user, 'cvgops'
+set :group, 'cvgops'
+set :rvm_type, :system
+server '192.168.15.2', :app, :web, :db, :primary => true
