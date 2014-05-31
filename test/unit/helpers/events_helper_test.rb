1ce82dbc98f7b217c57cbec2ebb17371f5d31edf
Michael Scott Shappe <mshappe@camelopard-consulting.com>
2014-05-31 16:29:10 -0500

    Active counts no longer include secure-actives.
    Story: https://www.pivotaltracker.com/story/show/54413446

--- test/unit/helpers/events_helper_test.rb
+++ test/unit/helpers/events_helper_test.rb
@@ -1,4 +1,46 @@
 require 'test_helper'
 
 class EventsHelperTest < ActionView::TestCase
+  include EventsHelper
+
+  setup do
+    FactoryGirl.create :ordinary_event
+    FactoryGirl.create :ordinary_event, secure: true
+  end
+
+  user_context :typical_context do
+     context '#get_active_count' do
+      should 'return 1 non-secure event' do
+        assert_equal 1, get_active_count
+      end
+    end
+
+    context '#get_secure_count' do
+      setup do
+        self.expects(:current_user).returns(@user)
+      end
+
+      should 'return 0 because we are not cool enough' do
+        assert_equal 0, get_secure_count
+      end
+    end
+  end
+
+  user_context :admin_context do
+    context '#get_active_count' do
+      should 'return 1 events' do
+        assert_equal 1, get_active_count
+      end
+    end
+
+    context '#get_secure_count' do
+      setup do
+        self.expects(:current_user).returns(@admin)
+      end
+
+      should 'return 1 event' do
+        assert_equal 1, get_secure_count
+      end
+    end
+  end
 end
