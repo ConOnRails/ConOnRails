1aa0d46b52e7106817c6f4379cff48ad50adfedb
Michael Scott Shappe <mshappe@camelopard-consulting.com>
2015-06-25 19:08:30 -0500

    GAH
    Former-commit-id: 93d00f98e77c9782fbf7f0b684ba33bc5cd80fa7

---
+++ db/migrate/20150626000353_fix_event_flag_defaults.rb
@@ -0,0 +1,10 @@
+class FixEventFlagDefaults < ActiveRecord::Migration
+  def change
+    change_column :events, :post_con, :boolean, default: false
+    change_column :events, :sticky, :boolean,  default: false
+    change_column :events, :emergency, :boolean, default: false
+    change_column :events, :medical, :boolean, default: false
+    change_column :events, :hidden, :boolean, default: false
+    change_column :events, :secure, :boolean, default: false
+  end
+end
