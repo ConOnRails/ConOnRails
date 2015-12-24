9d31ef6526a5a91906c0e3ad103859880c986667
Michael Scott Shappe <mshappe@camelopard-consulting.com>
2015-12-23 19:22:01 -0600

    Snapshot

--- test/models/section_role_test.rb
+++ test/models/section_role_test.rb
@@ -5,4 +5,5 @@ class SectionRoleTest < ActiveSupport::TestCase
   should belong_to :role
   should validate_presence_of :permission
   should validate_uniqueness_of(:permission).scoped_to([:section_id, :role_id])
+  should validate_inclusion_of(:permission).in_array( %w[read read_secure write] )
 end
