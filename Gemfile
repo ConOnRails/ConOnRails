6f8700fd63bf3c76a4d9abeca01fc9c9749e0fbb
Michael Shappe <mshappe@camelopard-consulting.com>
2023-06-14 21:04:32 +0000

    To github

--- Gemfile
+++ Gemfile
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 source 'http://rubygems.org'
-ruby '3.1.3'
+ruby '3.1.4'
 
 # Ruby decoupled gems
 gem 'net-smtp'
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -53,9 +53,9 @@ module ApplicationHelper
   def markdown(text)
     return if text.blank?
 
-    options = [:hard_wrap, :autolink, :no_intra_emphasis, :fenced_code_blocks, :underline, :highlight,
-               :no_images, :filter_html, :safe_links_only, :prettify, :no_styles]
-    Markdown.new(text, *options).to_html.html_safe
+    options = %i[hard_wrap autolink no_intra_emphasis fenced_code_blocks underline highlight
+                 no_images filter_html safe_links_only prettify no_styles]
+    Markdown.new(text, *options).to_html.html_safe # rubocop:disable Rails/OutputSafety
   end
 
   def show_corkboard(tag)
